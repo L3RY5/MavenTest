000f7a9bdb7e068a14e6a3d91ff21b3ba1a406b4
L3RY5 <lerys91@hotmail.com>
2018-09-24 11:40:19 +0200

    initial commit pof our maven Git project

--- src/main/java/com/realdolmen/maven/gitmaven/GitClass.java
+++ src/main/java/com/realdolmen/maven/gitmaven/GitClass.java
@@ -5,4 +5,9 @@ public class GitClass {
     public void test() {
         System.out.println("Hello Git World!");
     }
-}
+    
+    public String TextToEric()
+    {
+       return "Lukt het nog steeds niet om naar de andere kant te springen???";
+    }
+            }
