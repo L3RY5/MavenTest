00d59c8f2283c0de5efd4d6eca53299410f131fb
L3RY5 <lerys91@hotmail.com>
2018-09-24 10:31:28 +0200

    initial commit pof our maven Git project

---
+++ NetBeansProjects/gitMaven/src/main/java/com/realdolmen/com/gitmaven/GitClass.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.realdolmen.com.gitmaven;
+
+/**
+ *
+ * @author lerys
+ */
+public class GitClass {
+
+    public GitClass() {
+        System.out.println("Hello Git 1word");
+    }
+    
+}
